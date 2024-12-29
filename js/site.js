fb54f38f8754eacc8f579aa443edbc99375590a8
Fadjhart <121946184+Fadjhart@users.noreply.github.com>
2024-12-29 09:31:45 +0700

    update

--- js/site.js
+++ js/site.js
@@ -1,67 +1,73 @@
 "use strict";
 
 $(document).ready(function () {
-	/* Video Lightbox */
-	if (!!$.prototype.simpleLightboxVideo) {
-		$('.video').simpleLightboxVideo();
-	}
+    /* Video Lightbox */
+    if (!!$.prototype.simpleLightboxVideo) {
+        $('.video').simpleLightboxVideo();
+    }
 
-	/*ScrollUp*/
-	if (!!$.prototype.scrollUp) {
-		$.scrollUp();
-	}
+    /* ScrollUp */
+    if (!!$.prototype.scrollUp) {
+        $.scrollUp();
+    }
 
-	/*Responsive Navigation*/
-	$("#nav-mobile").html($("#nav-main").html());
-	$("#nav-trigger span").on("click",function() {
-		if ($("nav#nav-mobile ul").hasClass("expanded")) {
-			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
-			$(this).removeClass("open");
-		} else {
-			$("nav#nav-mobile ul").addClass("expanded").slideDown(250);
-			$(this).addClass("open");
-		}
-	});
+    /* Responsive Navigation */
+    $("#nav-mobile").html($("#nav-main").html()); // Duplikasi menu utama ke menu mobile
 
-	$("#nav-mobile").html($("#nav-main").html());
-	$("#nav-mobile ul a").on("click",function() {
-		if ($("nav#nav-mobile ul").hasClass("expanded")) {
-			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
-			$("#nav-trigger span").removeClass("open");
-		}
-	});
+    // Default state of mobile menu should be closed
+    $("nav#nav-mobile ul").removeClass("expanded").hide(); // Menu starts closed
+    $("#nav-trigger").removeClass("open"); // Ensure hamburger is in its default state
 
-	/* Sticky Navigation */
-	if (!!$.prototype.stickyNavbar) {
-		$('#header').stickyNavbar();
-	}
+    $("#nav-trigger").on("click", function () {
+        const $navMobile = $("nav#nav-mobile ul");
 
-	$('#content').waypoint(function (direction) {
-		if (direction === 'down') {
-			$('#header').addClass('nav-solid fadeInDown');
-		}
-		else {
-			$('#header').removeClass('nav-solid fadeInDown');
-		}
-	});
+        if ($navMobile.hasClass("expanded")) {
+            $navMobile.removeClass("expanded").slideUp(250); // Tutup menu dengan efek slide
+            $(this).removeClass("open"); // Hapus animasi trigger
+        } else {
+            $navMobile.addClass("expanded").slideDown(250); // Buka menu dengan efek slide
+            $(this).addClass("open"); // Tambahkan animasi trigger
+        }
+    });
 
+    /* Tutup Menu Mobile Setelah Klik Link */
+    $("#nav-mobile ul a").on("click", function () {
+        const $navMobile = $("nav#nav-mobile ul");
+
+        if ($navMobile.hasClass("expanded")) {
+            $navMobile.removeClass("expanded").slideUp(250); // Tutup menu setelah klik
+            $("#nav-trigger").removeClass("open"); // Reset animasi trigger
+        }
+    });
+
+    /* Sticky Navigation */
+    if (!!$.prototype.stickyNavbar) {
+        $('#header').stickyNavbar();
+    }
+
+    /* Navigasi Solid saat Scroll */
+    $('#content').waypoint(function (direction) {
+        if (direction === 'down') {
+            $('#header').addClass('nav-solid fadeInDown');
+        } else {
+            $('#header').removeClass('nav-solid fadeInDown');
+        }
+    });
 });
 
+/* Preloader and Animations */
+$(window).on('load', function () { // Pastikan seluruh elemen dimuat
+    $('#status').fadeOut(); // Menghilangkan animasi loading
+    $('#preloader').delay(350).fadeOut('slow'); // Menghilangkan overlay preloader
+    $('body').delay(350).css({ 'overflow-y': 'visible' });
 
-/* Preloader and animations */
-$(window).load(function () { // makes sure the whole site is loaded
-	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(350).css({'overflow-y': 'visible'});
+    /* WOW Elements */
+    if (typeof WOW === 'function') {
+        new WOW().init();
+    }
 
-	/* WOW Elements */
-	if (typeof WOW == 'function') {
-		new WOW().init();
-	}
-
-	/* Parallax Effects */
-	if (!!$.prototype.enllax) {
-		$(window).enllax();
-	}
-
+    /* Parallax Effects */
+    if (!!$.prototype.enllax) {
+        $(window).enllax();
+    }
 });
